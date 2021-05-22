f0fc15144ee620368ba2331217e5efbbc5bd196b
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-05-22 15:20:47 +0200

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,56 @@
 <template>
   <Explorer>
-    <v-row class="height">
-      <v-col cols="4" class="d-flex justify-center align-center">
-        <v-card>
-          <v-img
-            src="@/assets/adam_avatar.png"
-            class="white--text align-end"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+    <v-row class="height justify-center align-center">
+      <v-col cols="4" class="flip-card d-flex justify-center align-center">
+        <v-card class="flip-card-inner">
+          <div
+            class="flip-card-front d-flex flex-column justify-center align-center"
           >
-            <v-card-title>El Omari Adam</v-card-title>
-          </v-img>
-
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <span class="text-center font-weight-bold">Web Developer</span>
-          </v-card-actions>
+            <v-img
+              src="@/assets/adam_avatar.png"
+              class="white--text align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            >
+            </v-img>
+          </div>
+          <div class="flip-card-back">
+            <h1>El Omari Adam <v-spacer></v-spacer>Web Developer</h1>
+            <p class="ma-3">
+              <v-row>
+                <v-col cols="12"
+                  >What I <v-icon color="white"> mdi-heart </v-icon></v-col
+                >
+                <v-col
+                  cols="4"
+                  class="d-flex justify-center align-center flex-column"
+                >
+                  <span>Football</span>
+                  <v-icon large color="white"> mdi-soccer </v-icon>
+                </v-col>
+                <v-col
+                  cols="4"
+                  class="d-flex justify-center align-center flex-column"
+                >
+                  <span>Greek Mythology</span>
+                  <v-icon large color="white"> mdi-lightning-bolt </v-icon>
+                </v-col>
+                <v-col
+                  cols="4"
+                  class="d-flex justify-center align-center flex-column"
+                >
+                  <span>Detective Novels</span>
+                  <v-icon large color="white"> mdi-book </v-icon>
+                </v-col>
+              </v-row>
+            </p>
+            <h2 class="mt-5">Don't hesitate to contact me !!!</h2>
+          </div>
         </v-card>
       </v-col>
       <v-col cols="8" class="d-flex justify-center align-center">
         <v-card>
           <v-card-text>
-            <div class="text--primary">
+            <div class="text--primary font-weight-bold">
               Hello &#128587; !!! My name is Adam and I'm a FullStack Web
               Developer <br />
               This is my portfolio where you can find my projects. <br />
@@ -56,3 +86,38 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.flip-card {
+  width: 80%;
+  height: 80%;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-back {
+  background-image: linear-gradient(to right top, #004d7a, #0ea6b4);
+  color: white;
+  transform: rotateY(180deg);
+}
+</style>
