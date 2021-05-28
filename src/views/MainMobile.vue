d14a6aa0a18dc8583c6bc7dfdc66da1c18df87e7
AdamELO <59090327+AdamELO@users.noreply.github.com>
2021-05-28 17:18:25 +0200

    mobile ui home end

--- src/views/MainMobile.vue
+++ src/views/MainMobile.vue
@@ -14,10 +14,42 @@
           <div class="fontHour">12:30</div>
           <div class="fontDate">Friday 28 Mei</div>
         </v-col>
-        <v-col cols="4" class="d-flex align-center justify-center">
+        <v-col cols="4" class="d-flex align-center">
           <v-icon x-large color="white">mdi-weather-partly-cloudy</v-icon>
           <v-spacer></v-spacer>
           <span class="fontWeather">17°</span>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container fluid class="mobileMenu">
+      <v-row class="black--text font-weight-bold">
+        <v-col cols="3" class="d-flex flex-column justify-center align-center">
+          <v-img src="@/assets/about.png"></v-img>
+          <span>About</span>
+        </v-col>
+        <v-col cols="3" class="d-flex flex-column justify-center align-center">
+          <v-img src="@/assets/skillsMobile.png"></v-img>
+          <span>Skills</span>
+        </v-col>
+        <v-col cols="3" class="d-flex flex-column justify-center align-center">
+          <v-img src="@/assets/gallery.png"></v-img>
+          <span>Projects</span>
+        </v-col>
+        <v-col cols="3" class="d-flex flex-column justify-center align-center">
+          <v-img src="@/assets/camera.png"></v-img>
+          <span>Camera</span>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="@/assets/phoneMobile.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="@/assets/messagesMobile.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="@/assets/linkedin_64px.png"></v-img>
+        </v-col>
+        <v-col cols="3">
+          <v-img src="@/assets/github.png"></v-img>
         </v-col>
       </v-row>
     </v-container>
@@ -51,5 +83,9 @@ export default {
 }
 .fontDate {
   font-size: 1.2rem;
+}
+.mobileMenu {
+  position: absolute;
+  bottom: 60px;
 }
 </style>
